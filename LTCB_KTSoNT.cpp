369ecc418db2fafab0789a4a64c6090f59deb258
TamNhien <canhthien91@gmail.com>
2024-03-16 09:39:42 +0700

    Add project files.

---
+++ LTCB_KTSoNT.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "Nhap mot so nguyen duong n: ";
+    cin >> n;
+
+    cout << n << (isPrime(n) ? " la so nguyen to." : " khong phai la so nguyen to.") << endl;
+
+    return 0;
+}
